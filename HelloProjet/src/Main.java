91b3098b20e99717c6668b2098e6925c4857f6a2
YuBin Wang <miniboywyb@qq.com>
2018-08-01 10:37:38 +0800

    这个我得第一次修改

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("这个是我自己修改的第一个版本");
+        System.out.println("这是我的第二个版本");
     }
 }
