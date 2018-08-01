4d74fa3f50586ec59c7341b4457b9d53e382c080
YuBin Wang <miniboywyb@qq.com>
2018-08-01 13:39:02 +0800

    制造冲突 12323

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -4,10 +4,6 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
-        System.out.println("这个是我自己修改的第一个版本");
-        System.out.println("这是我的第二个版本");
-        System.out.println("这是制造冲突的版本7");
-        System.out.println("看看这个会不会自动的合并");
+        System.out.println("master版本");
     }
 }
